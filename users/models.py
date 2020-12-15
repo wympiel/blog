d16baef3a258c3a3c2d06cfa2c5f7f8aa9a45bb5
wympiel <wymp10@gmail.com>
2020-12-15 22:16:39 +0100

    fifth commit

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    suer = models.OneToOneField(User, on_delete=models.CASCADE)
 
 # Create your models here.
